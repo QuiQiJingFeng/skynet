95abda3be70fcefabfa940ca82ae56e9e50a1042
JingFeng <gejinnian224@163.com>
2017-06-04 00:03:41 +0800

    添加注释

--- skynet-src/skynet_env.c
+++ skynet-src/skynet_env.c
@@ -14,9 +14,10 @@ struct skynet_env {
 };
 
 static struct skynet_env *E = NULL;
-
+//从虚拟机中获取全局变量
 const char * 
 skynet_getenv(const char *key) {
+	//对skynet_env 加锁,如果已经有锁，则等待直到锁被解开后才往下执行
 	SPIN_LOCK(E)
 
 	lua_State *L = E->L;
@@ -24,18 +25,30 @@ skynet_getenv(const char *key) {
 	lua_getglobal(L, key);
 	const char * result = lua_tostring(L, -1);
 	lua_pop(L, 1);
-
+	//对skynet_env 解锁
 	SPIN_UNLOCK(E)
 
 	return result;
 }
-
+//向虚拟机中写入全局变量 
+/*
+	Lua堆栈
+	   栈顶
+	----------
+  -1|        |4
+  -2|        |3
+  -3|        |2
+  -4|        |1
+	----------
+	   栈底
+*/
 void 
 skynet_setenv(const char *key, const char *value) {
 	SPIN_LOCK(E)
 	
 	lua_State *L = E->L;
 	lua_getglobal(L, key);
+	//FYD 由此可知 一个值如果赋值两遍要崩掉的
 	assert(lua_isnil(L, -1));
 	lua_pop(L,1);
 	lua_pushstring(L,value);
@@ -43,7 +56,7 @@ skynet_setenv(const char *key, const char *value) {
 
 	SPIN_UNLOCK(E)
 }
-
+//初始化skynet_env  创建一个虚拟机
 void
 skynet_env_init() {
 	E = skynet_malloc(sizeof(*E));
--- skynet-src/spinlock.h
+++ skynet-src/spinlock.h
@@ -5,40 +5,51 @@
 #define SPIN_LOCK(q) spinlock_lock(&(q)->lock);
 #define SPIN_UNLOCK(q) spinlock_unlock(&(q)->lock);
 #define SPIN_DESTROY(q) spinlock_destroy(&(q)->lock);
-
+//如果没有定义pthread 锁 那么使用下面的
 #ifndef USE_PTHREAD_LOCK
 
 struct spinlock {
 	int lock;
 };
-
+//初始化spinlock state 0 代表无锁
 static inline void
 spinlock_init(struct spinlock *lock) {
 	lock->lock = 0;
 }
-
+/*
+	gcc提供的用于原子操作的方法：将*ptr设为value并返回*ptr操作之前的值。
+	type __sync_lock_test_and_set (type *ptr, type value, ...)
+*/
+/*
+	将lock的 state状态设置为锁定状态,循环锁定，直到锁释放为止 
+	百科:http://baike.baidu.com/item/%E8%87%AA%E6%97%8B%E9%94%81
+*/
 static inline void
 spinlock_lock(struct spinlock *lock) {
 	while (__sync_lock_test_and_set(&lock->lock,1)) {}
 }
-
+//尝试增加锁状态,并返回之是否成功锁定,因为如果之前为0代表无锁状态
 static inline int
 spinlock_trylock(struct spinlock *lock) {
 	return __sync_lock_test_and_set(&lock->lock,1) == 0;
 }
-
+/*
+	void __sync_lock_release (type *ptr, ...)
+     将*ptr置0
+*/
+//接触锁定状态
 static inline void
 spinlock_unlock(struct spinlock *lock) {
 	__sync_lock_release(&lock->lock);
 }
-
+//听别人说(void) lock是为了避免警告,不太理解  先标记下
 static inline void
 spinlock_destroy(struct spinlock *lock) {
 	(void) lock;
 }
 
 #else
-
+//如果定义了USE_PTHREAD_LOCK 那么使用下面的pthread库
 #include <pthread.h>
 
 // we use mutex instead of spinlock for some reason
@@ -47,7 +58,7 @@ spinlock_destroy(struct spinlock *lock) {
 struct spinlock {
 	pthread_mutex_t lock;
 };
-
+//初始化pthread库
 static inline void
 spinlock_init(struct spinlock *lock) {
 	pthread_mutex_init(&lock->lock, NULL);
