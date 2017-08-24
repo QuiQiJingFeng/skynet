e19dd806a15f4471808446ed154373cf8ce853cf
mu77 <Zhaoqinglong@Mu77.com>
2017-08-24 16:03:24 +0800

    test udp

--- examples/main.lua
+++ examples/main.lua
@@ -1,22 +1,41 @@
 local skynet = require "skynet"
-local sprotoloader = require "sprotoloader"
+local socket = require "skynet.socket"
 
-local max_client = 64
+local NUMBER = 1000000
+
+local function server()
+	local tb = {}
+	local host
+	host = socket.udp(function(str, from)
+		-- print("server recv", str, socket.udp_address(from))
+		
+		if str == "END" then
+			for i=1,NUMBER do
+				if not tb[i] then
+					print("MISS: ",i)
+				end
+			end
+			print("FYD=====END")
+		else
+			tb[tonumber(str)] = tonumber(str)
+			socket.sendto(host, from, str)
+		end
+	end , "127.0.0.1", 8888)	-- bind an address
+end
+
+-- local function client()
+-- 	local c = socket.udp(function(str, from)
+-- 		-- print("client recv", str, socket.udp_address(from))
+-- 	end)
+-- 	socket.udp_connect(c, "47.52.99.120:8888", 8888)
+
+-- 	for i=1,NUMBER do
+-- 		socket.write(c, tostring(i))	-- write to the address by udp_connect binding
+-- 	end
+-- 	socket.write(c, "END")
+-- end
 
 skynet.start(function()
-	skynet.error("Server start")
-	skynet.uniqueservice("protoloader")
-	if not skynet.getenv "daemon" then
-		local console = skynet.newservice("console")
-	end
-	skynet.newservice("debug_console",8000)
-	skynet.newservice("simpledb")
-	local watchdog = skynet.newservice("watchdog")
-	skynet.call(watchdog, "lua", "start", {
-		port = 8888,
-		maxclient = max_client,
-		nodelay = true,
-	})
-	skynet.error("Watchdog listen on", 8888)
-	skynet.exit()
+	skynet.fork(server)
+	-- skynet.fork(client)
 end)
